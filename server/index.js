271b70fcd1b52878447b71dd8b35f65ba1d179eb
Ana Luisa Leão <analeao21@gmail.com>
2023-09-17 10:55:24 -0300

    Adicionando IA

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 
+import { convert } from "./convert.js"
 import { download } from "./download.js"
 import { trancribe } from "./transcribe.js"
 import { summarize } from "./summarize.js"
@@ -10,13 +11,26 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
-  await download(request.params.id)
-  const result = await trancribe()
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    const result = await trancribe(audioConverted)
 
-  return response.json({ result })
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
 })
+
 app.post("summary", async (request, response) => {
-  const result = await summarize(request.body.text)
-  return response.json({ result })
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
 })
+
 app.listen(3333, () => console.log("Server is running on port 3333"))
--- server/transcribe.js
+++ server/transcribe.js
@@ -1,4 +1,22 @@
+import { pipeline } from "@xenova/transformers"
+
 import { transcriptionExample } from "./utils/transcription.js"
-export async function trancribe() {
-  return transcriptionExample
+export async function trancribe(audio) {
+  try {
+    console.log("Realizando a transcrição...")
+    const trancribe = await pipeline(
+      "automatic-speech-recognition",
+      "Xenova/whisper-small"
+    )
+    const transcription = await trancribe(audio, {
+      chunk_length_s: 30,
+      stride_length_s: 5,
+      language: "portuguese",
+      task: "transcribe",
+    })
+    console.log("Transcrição finalizada com sucesso!")
+    return transcription?.text.replace("[Música]", "")
+  } catch (error) {
+    throw new Error(error)
+  }
 }
